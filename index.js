9882fb088168e6b063fe10b39bb635184e71c0f9
Kossy-Augusta <kossyudogu@gmail.com>
2024-11-28 01:25:29 +0100

    updated index wih buitlin middlewares and route definations

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const connectDB = require('./config/dbCon');
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const cors = require('cors')
+const corsOptions = require('./config/cos-options')
 const PORT = process.env.PORT || 3600
-
+// connect DB
 connectDB();
+app.use(express.urlencoded({ extended: false }));
+// cross origin resource sharing
+app.use(cors(corsOptions));
+// json middleware
+app.use(express.json());
+
+app.use("/signup", require("./routes/api"));
 
 mongoose.connection.once('open', ()=>{
     console.log('connected to mongoDB');
