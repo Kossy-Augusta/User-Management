714db8cb7a7111ad956973a78ded8d50f43830e3
Kossy-Augusta <kossyudogu@gmail.com>
2024-11-28 15:15:00 +0100

    code fixes

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const ValidatorSchema = require("../utils/validator-schema");
+const validateReqBody = require('../middleware/validation')
 const UserController = require('../controllers/authController');
 
+const validator = new ValidatorSchema();
 const userController = new UserController();
-router.post('/', userController.create)
+router.post("/",validateReqBody(validator.createUserSchema), userController.create.bind(userController));
 
 module.exports = router
