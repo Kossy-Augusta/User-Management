fdb1246a87840c09d9c7ae066fb5a0695e6c6539
Kossy-Augusta <kossyudogu@gmail.com>
2024-12-04 16:22:48 +0100

    fixes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,131 +1,140 @@
-const User = require('../model/User');
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const sendResponse = require('../utils/sendResponse')
+const User = require("../model/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const sendResponse = require("../utils/sendResponse");
 
-class UserController{
-    async create(req, res){
-        try {
-            const {email, first_name, last_name,password} = req.body
-            // check for duplicate
-            const existingUser = await User.findOne({email}).exec();
-            if(existingUser)return res.status(409).send({error: true,'message':'Email is already taken'});
-            const hashpwd = await bcrypt.hash(password, 10);
-            const newUser = await User.create({
-                email,
-                first_name,
-                last_name,
-                password: hashpwd,
-            })
-            const data = {
-                _id: newUser._id,
-                email : newUser.email,
-                first_name: newUser.first_name,
-                last_name: newUser.last_name
-            }
-            sendResponse(res, 201, `New user created!!`, data);
-        } catch (err) {
-            console.log(err.message);
-            res.status(err.status || 500).json({ error: true, message: err.message });
-        }
+class UserController {
+  async create(req, res) {
+    try {
+      const { email, first_name, last_name, password } = req.body;
+      // check for duplicate
+      const existingUser = await User.findOne({ email }).exec();
+      if (existingUser)
+        return res
+          .status(409)
+          .send({ error: true, message: "Email is already taken" });
+      const hashpwd = await bcrypt.hash(password, 10);
+      const newUser = await User.create({
+        email,
+        first_name,
+        last_name,
+        password: hashpwd,
+      });
+      const data = {
+        _id: newUser._id,
+        email: newUser.email,
+        first_name: newUser.first_name,
+        last_name: newUser.last_name,
+      };
+      sendResponse(res, 201, `New user created!!`, data);
+    } catch (err) {
+      console.log(err.message);
+      res.status(err.status || 500).json({ error: true, message: err.message });
     }
+  }
 
-    async login(req, res){
-        const {email, password} = req.body
-        // check if user exist
-        try {
-            const currentUser = await User.findOne({ email }).exec();
-            if(!currentUser) return res.status(400).send({error: true, message: "Unauthorized access"});
+  async login(req, res) {
+    const { email, password } = req.body;
+    // check if user exist
+    try {
+      const currentUser = await User.findOne({ email }).exec();
+      if (!currentUser)
+        return res
+          .status(400)
+          .send({ error: true, message: "Unauthorized access" });
 
-            const verifyPassword = await bcrypt.compare(password, currentUser.password);
-            if (!verifyPassword) return res.status(401).send({error:true, message: "Incorrect password"});
-            const roles = currentUser.roles;
-            
-            // create JWT
-            const accessToken = jwt.sign(
-                {
-                    userInfo: {
-                        first_name: currentUser.first_name,
-                        last_name: currentUser.last_name,
-                        roles
-                    }
-                },
-                process.env.ACCESS_TOKEN_SECRET,
-                {expiresIn: '10m'}
-            );
-            const refreshToken = jwt.sign(
-                {
-                    userInfo: {
-                        first_name: currentUser.first_name,
-                        last_name: currentUser.last_name,
-                        roles: roles
-                    }
-                },
-                process.env.REFRESH_TOKEN_SECRET,
-                {expiresIn: '1d'}
-            );
+      const verifyPassword = await bcrypt.compare(
+        password,
+        currentUser.password
+      );
+      if (!verifyPassword)
+        return res
+          .status(401)
+          .send({ error: true, message: "Incorrect password" });
+      const roles = currentUser.roles;
 
-            // save refresh token to db with current user
-            
-            currentUser.refresh_token = refreshToken;
-            await currentUser.save();
-            // set the cookie with  the refresh token
-            res.cookie(
-                "jwt", 
-                refreshToken,
-                {
-                    httpOnly: true,
-                    sameSite: "None",
-                    // secure: true,
-                    maxAge: 24 * 60 * 60 * 1000
-                }
-            )
-            const data = [{access_token: accessToken},{ 
-                _id: currentUser._id,
-                first_name: currentUser.first_name,
-                last_name: currentUser.last_name,
-            }]
-            sendResponse(res, 200, `User login Successful`, data)
-        } catch (error) {
-            const statusCode = error.statusCode || 500;
-            sendResponse(res, statusCode, error.message, null, true);
-        }
+      // create JWT
+      const accessToken = jwt.sign(
+        {
+          userInfo: {
+            first_name: currentUser.first_name,
+            last_name: currentUser.last_name,
+            roles,
+          },
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "10m" }
+      );
+      const refreshToken = jwt.sign(
+        {
+          userInfo: {
+            first_name: currentUser.first_name,
+            last_name: currentUser.last_name,
+            roles: roles,
+          },
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: "1d" }
+      );
+
+      // save refresh token to db with current user
+
+      currentUser.refresh_token = refreshToken;
+      await currentUser.save();
+      // set the cookie with  the refresh token
+      res.cookie("jwt", refreshToken, {
+        httpOnly: true,
+        sameSite: "None",
+        // secure: true,
+        maxAge: 24 * 60 * 60 * 1000,
+      });
+      const data = [
+        { access_token: accessToken },
+        {
+          _id: currentUser._id,
+          first_name: currentUser.first_name,
+          last_name: currentUser.last_name,
+        },
+      ];
+      sendResponse(res, 200, `User login Successful`, data);
+    } catch (error) {
+      const statusCode = error.statusCode || 500;
+      sendResponse(res, statusCode, error.message, null, true);
     }
+  }
 
-    async logout(req, res){
-        const cookies = req.cookies;
-        if(!cookies?.jwt) return res.status(400).send({ message: "No cookie found" });
-        const refreshToken = cookies.jwt;
-        try {
-            const currentUser = await User.findOne({ refresh_token: refreshToken }).exec();;
-            
-            if(!currentUser){
-                res.clearCookie("jwt",
-                    {
-                        httpOnly: true,
-                        sameSite: "None",
-                        // secure: true,
-                    }
-                )
-                return res.status(204).send({message: "User is already logged out"})
-            }
-            currentUser.refresh_token = "";
-            const result = await currentUser.save();
-            res.clearCookie("jwt",
-                {
-                    httpOnly: true,
-                    sameSite: "None",
-                    // secure: true,
-                }
-        );
-            res.status(200).send({"message": "User logged out succesfully"});
-        } catch (err) {
-            const statusCode = err.statusCode || 500;
-            res.status(statusCode).send({message: err.message})
-        }
-        
+  async logout(req, res) {
+    const cookies = req.cookies;
+    if (!cookies?.jwt)
+      return res.status(400).send({ message: "No cookie found" });
+    const refreshToken = cookies.jwt;
+    try {
+      const currentUser = await User.findOne({
+        refresh_token: refreshToken,
+      }).exec();
+
+      if (!currentUser) {
+        res.clearCookie("jwt", {
+          httpOnly: true,
+          sameSite: "None",
+          // secure: true,
+        });
+        return res.status(204).send({ message: "User is already logged out" });
+      }
+      currentUser.refresh_token = "";
+      const result = await currentUser.save();
+      res.clearCookie("jwt", {
+        httpOnly: true,
+        sameSite: "None",
+        // secure: true,
+      });
+      res.status(200).send({ message: "User logged out succesfully" });
+    } catch (err) {
+      const statusCode = err.statusCode || 500;
+      res.status(statusCode).send({ message: err.message });
     }
-    async refreshToken(req, res){
+  }
+  async refreshToken(req, res){
         const cookies = req.cookies;
         if(!cookies?.jwt) return res.status(400).send({message: "No Cookie Found"});
         const refreshToken = cookies.jwt;
@@ -157,4 +166,4 @@ class UserController{
     }
 }
 
-module.exports = UserController
+module.exports = UserController;
--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const ValidatorSchema = require("../utils/validator-schema");
-const validateReqBody = require('../middleware/validation')
-const UserController = require('../controllers/authController');
-const verifyJWT = require('../middleware/verifyJWT')
+const validateReqBody = require("../middleware/validation");
+const UserController = require("../controllers/authController");
+const verifyJWT = require("../middleware/verifyJWT");
 
 const validator = new ValidatorSchema();
 const userController = new UserController();
-router.post("/register",validateReqBody(validator.createUserSchema), userController.create);
-router.post("/login",validateReqBody(validator.loginUserSchema), userController.login);
+router.post(
+  "/register",
+  validateReqBody(validator.createUserSchema),
+  userController.create
+);
+router.post(
+  "/login",
+  validateReqBody(validator.loginUserSchema),
+  userController.login
+);
 router.get("/logout", verifyJWT, userController.logout);
-router.get("/refresh", userController.refreshToken)
+router.get("/refresh", userController.refreshToken);
 
-module.exports = router
+module.exports = router;
